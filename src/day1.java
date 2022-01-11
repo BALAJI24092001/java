f1c950266d99d033030073edace7ee0ef61f1c7c
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-01-11 13:41:08 +0530

    created proj, day#1 e#1

---
+++ src/day1.java
@@ -0,0 +1,15 @@
+
+public class day1 {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		balaji();
+	}
+	
+	public static void balaji() {
+		System.out.println("Hello balaji!!");
+	}
+
+}
+
+

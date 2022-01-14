50994b4974d16b793ac47f5bd049d5dac37753b6
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-01-15 01:50:57 +0530

    day#4 e#1

---
+++ src/day4.java
@@ -0,0 +1,20 @@
+public class day3{
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+
+class Calc {
+	int n1;
+	int n2;
+	int res;
+		
+	public Calc(){ 
+		// A constructor has same name as the class
+		// A constructor does not return any value
+		// A constructor is automatically called when an object is created, and only once it is called through out the object's life  }
+
+	}
+}

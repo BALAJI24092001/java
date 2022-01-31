b1a94e4199329e7d058497f96d78eda12cd5514b
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-01-31 23:09:08 +0530

    Class names : CamelCaseNaming

---
+++ src/Day3.java
@@ -0,0 +1,106 @@
+public class day3{
+	public static void main(String[] args) {
+
+		// build a right triangle with sequential numbers
+		for (int i = 1; i <= 6; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j + " "); // replace j with "*" for * based right triangle pattern
+			}
+			System.out.println();
+		}
+		// GOOD MORNING
+		// output :
+		// 1 
+		// 1 2 
+		// 1 2 3 
+		// 1 2 3 4 
+		// 1 2 3 4 5 
+		// 1 2 3 4 5 6 
+
+		System.out.println();
+
+		int sum=0;
+		for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                sum+=1;
+                System.out.print(sum+" ");
+            }
+            System.out.println(" ");
+		}
+		// adjust the code with 2 spaces for single digits and 1 space for double for better presentation.
+		//output
+		//1
+		//2  3
+		//4  5  6
+		//7  8  9  10
+		//11 12 13 14 15
+		
+		// build a right triangle with capital alphabets
+		char temp = 'A';
+		for (int i = temp; i < (int)temp + 5; i++) {
+			for (int j = temp; j <= i; j++) {
+				System.out.print((char)j + " ");
+			}
+			System.out.println();
+		}
+		// output :
+		// A 
+		// A B 
+		// A B C 
+		// A B C D 
+		// A B C D E
+
+		System.out.println();
+
+		//  build a squre box with "$" as border
+		int max = 4; // use a input function
+		for (int i = 1; i <= max; i++) {
+			if(i == 1 || i == max){
+				for (int j = 1; j <= max; j++) {
+					System.out.print("$ ");
+				}
+				System.out.println();
+			}
+			else{
+				System.out.print("$ ");
+				for (int j = 1; j <= (max - 2); j++) {
+					System.out.print("  ");
+				}
+				System.out.println("$ ");
+			}
+		}
+		// output :
+		// $ $ $ $ 
+		// $     $ 
+		// $     $ 
+		// $ $ $ $ 
+		
+		System.out.println(); // break and continue escape statements similar to cpp
+		// in simple statements: 
+		// break : To break the loop
+		// continue : To skip an iteration of loop
+
+		// OOP instance and test
+		Calc obj; // creating a reference(address at memorey)
+		obj = new Calc(); // allocating memory to the ref. Now obj is an object.
+		obj.c1 = 10;
+		obj.c2 = 'B';
+		obj.calFun();
+		System.out.println("result value of the Calc class instance obj is : " + obj.result);
+
+
+	}
+}
+
+// 			OOP
+// ------------------------------------------------------------
+class Calc {
+	int c1 = 0; // while cpp assigns garbage value to un initialized variables, java assigns with the value 0. Garbage values of the variables are cleaned internally by java.(Garbage collection concept)
+	char c2 = 'A'; // "" are for string data type, '' are for charecter type.
+	int result;
+	public void calFun(){
+		result = c1 + (int)c2;
+	}
+}
---
+++ src/Day6.java
@@ -0,0 +1,108 @@
+public class day6{
+	public static void main(String[] args) {
+		
+
+		/* ARRAYS */
+
+
+		// one dimensional array
+		int[] intarr; // declaring an array
+		intarr = new int[3]; // instantiating an array
+		int[] intarr1 = new int[]{5, 6, 7, 8}; // declaration of an array using array literals
+		// also combining both declaration and instantiating statements
+		for (int i = 0; i < 3; i++) {
+			intarr[i] = i;
+		}
+		for (int var : intarr) {
+			System.out.println(var);
+			// using for each loop to print values from an array
+			// There are disadvantages in using for each loop
+			// 1. We cannot find the array index of the array value
+			// 2. We cannot update the array value
+			// 3, Cannot itterate the array from back
+			// 4. Have low performance issues
+		}
+		for (int i = 0; i < 3; i++) {
+			System.out.println(intarr[i]);
+			// using for loop
+		}
+
+
+		// array of non-primitive data types(objects of class Student)
+		Student[] stuArr = new Student[3];
+		String[] names = new String[]{"Balaji", "Java", "cpp"};
+		int[] rollNo = new int[]{1, 2, 3};
+		for (int i = 0; i < names.length; i++) {
+			stuArr[i] = new Student(rollNo[i], names[i]);
+		}
+		for (Student var : stuArr) {
+			System.out.println("Name : " + var.name + " RollNo : " + var.roll_no);
+		}
+		System.out.println();
+		for (int var : test()) { // for each loop justifing array returned by test function
+			System.out.print(var + "  ");
+		}
+		System.out.println();
+		System.out.println();
+
+
+		// two dimensional array
+		int[][] twoarr = { {1, 2, 3}, {4, 5, 6} };
+//		System.out.println(twoarr.length);
+		for (int i = 0; i < twoarr.length; i++) {
+			for (int j = 0; j < twoarr[0].length; j++) {
+				System.out.print(twoarr[i][j] + "  ");
+			}
+			System.out.println();
+		}
+
+
+		// three dimensional array
+		int[][][] threearr = { { {1, 2}, {3, 4} }, 
+				       { {5, 6}, {6, 7} } };
+		System.out.println();
+		System.out.println();
+		for (int[][] var : threearr) {
+			for (int i = 0; i < var.length; i++) {
+				for (int j = 0; j < var[0].length; j++) {
+					System.out.print(var[i][j] + "  ");
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+
+		// jagged array
+		int jagarr[][] = new int[][] {
+			new int[]{1, 2, 3},
+			new int[]{4, 5},
+			new int[]{6, 7, 8, 9, 10}
+		};
+
+		for (int[] var : jagarr) {
+			for (int var1: var) {
+				System.out.print(var1 + "  ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+	}
+	// a fucntion returning an array.
+	public static int[] test(){ // test function created outside of the main function but inside of the main class.
+		return new int[]{1, 2, 3, 4};
+	}
+}
+
+
+class Student{
+    public int roll_no;
+    public String name;
+    public Student(int roll_no, String name)
+    {
+        this.roll_no = roll_no;
+        this.name = name;
+    }
+}

ebea296ba02e46784f156c149d2949092c8bd059
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-02-05 23:55:03 +0530

    added fromatting

--- src/Day1.java
+++ src/Day1.java
@@ -1,5 +1,5 @@
-
-public class day1 {
+import java.text.DecimalFormat;
+public class Day1 {
 	
 	public static void main(String args[]) {
 		System.out.println("Hello World");
@@ -39,15 +39,52 @@ public class day1 {
 		int r1 = m + n;
 		int r2 = m - n;
 		System.out.print("Sum of m and n is: m+n = " + r1);
+		System.out.println();
 		System.out.print("Sum of m and n is: m+n = " + r2);
 		// pre increment and post increment, same as cpp.
 
 // break
 		balaji();
-	}
+		
+
+
+		// String format function
+		
+		// Format specifies are similar to C and C++
+		String str1 = String.format("%d", 101);  
+		String str2 = String.format("|%10d|", 101);  // Specifying length of integer // right justified to the length 
+		String str3 = String.format("|%-10d|", 101); // Left-justifying within the specified width  
+		String str4 = String.format("|% d|", 101);   
+		String str5 = String.format("|%010d|", 101); // Filling with zeroes          
+
+		System.out.println(str1);  
+		System.out.println(str2);  
+		System.out.println(str3);  
+		System.out.println(str4);  
+		System.out.println(str5);  
+
+
+		// Decimal format class
+		//
+		double i = 123.56;
+		DecimalFormat ft = new DecimalFormat("##"); // without fraction part, aslo can use typecast to int
+		System.out.println(ft.format(i)); // also restrict integer part to two places
+		ft = new DecimalFormat("###.#"); // restrict decimal part to one place
+		System.out.println(ft.format(i)); 
+		ft = new DecimalFormat("####.0000"); // appends zeros to right
+		System.out.println(ft.format(i));
+		ft = new DecimalFormat("00000.0000"); // formatting numeric part
+		System.out.println(ft.format(i));
+		ft = new DecimalFormat("$###,###.####"); // money number formatting
+		i = 123456.4321;
+		System.out.println(ft.format(i));
+}
 	
 	public static void balaji() {
+		System.out.println();
 		System.out.println("Hello balaji!!");
+		System.out.println();
+		System.out.println();
 	}
 	
 

df46ba35ce1d2b7e66961dd279d82807f67c8325
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-02-05 19:34:10 +0530

    que001 scanner util func

---
+++ ques/que001.java
@@ -0,0 +1,34 @@
+//In this challenge, you must read 3 integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
+
+//Sample input
+// 42
+// 100
+// 125
+
+import java.util.*;
+public class que001{
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		int a = scan.nextInt();
+		// Complete this line
+		int b = scan.nextInt();
+		// Complete this line
+		int c = scan.nextInt();
+
+		System.out.println(a);
+		// Complete this line
+		System.out.println(b);
+		// Complete this line
+		System.out.println(c);
+
+		scan.close();
+	}
+}
+
+
+// Sample output
+// 42
+// 100
+// 125

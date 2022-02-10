13a69876e03352c019c6119b6c39ea86fc025531
BALAJI24092001 <dbalajivaraprasad@gmail.com>
2022-02-10 22:17:33 +0530

    End-of-file problem

--- ques/que007.java
+++ ques/que007.java
@@ -1,4 +1,6 @@
 //Given an integer, N, print its first 10 multiples. Each multiple N x i (where 1<=i<=10) should be printed on a new line in the form: N x i = result.
+//
+// End-of-file problem
 
 
 import java.io.*;
